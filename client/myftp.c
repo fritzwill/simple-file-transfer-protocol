98f75e5414d3c764f261661b607a936e492b32ac
William Fritz <wfritz@student01.cse.nd.edu>
2018-10-04 02:57:52 -0400

    send/recieve messages

--- client/myftp.c
+++ client/myftp.c
@@ -13,11 +13,11 @@
 #include<sys/time.h>
 #include<unistd.h>
 
-#define BUF_SIZE 4096
+#define MAX_LINE 256
 
 int main(int argc, char * argv[]){
     int sockFd;
-    char buf[BUF_SIZE];
+    char buf[MAX_LINE];
 
     // check command line input
     if (argc != 3){
@@ -26,27 +26,58 @@ int main(int argc, char * argv[]){
     }
 
     // laod address structs
-    struct addrinfo hints, *res;
+    struct addrinfo hints, *clientInfo, *ptr;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
 
     // unputs: [IP or named addr, string(PORTNUM), struct addrinfo *, struct addrinfo **]
-    getaddrinfo(argv[1], argv[2], &hints, &res);
-
-    // make socket
-    if ((sockFd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0){
-        perror("bad socket");
-        exit(1);
-    }    
-
-    // make connect
-    if (connect(sockFd, res->ai_addr, res->ai_addrlen) < 0){
-        perror("bad connect");
-        close(sockFd);
+    int returnVal;
+    if ((returnVal = getaddrinfo(argv[1], argv[2], &hints, &clientInfo)) != 0){
+        fprintf(stderr, "getaddrinfo: %s\n", returnVal);
         exit(1);
     }
 
-    printf("sjda\n");
+    // loop through getaddrinfo results and connect to first we can
+    for (ptr = clientInfo; ptr != NULL; ptr = ptr->ai_next){
+        // make socket
+        if ((sockFd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol)) < 0){
+            perror("bad socket");
+            continue;
+        }    
+
+        // make connect
+        if (connect(sockFd, ptr->ai_addr, ptr->ai_addrlen) < 0){
+            perror("bad connect");
+            close(sockFd);
+            continue;
+        }
+        break; //found valid connect
+    }
+    
+    free(clientInfo); // don't need struct anymore
+    if (ptr == NULL){
+        fprintf(stderr, "failed to connect\n");
+        exit(1);
+    }
+    
+    // After connected, run main loop to get user inputs, send them to server, reacieve the results, then display
+    int lenSendString;
+    while (fgets(buf, sizeof(buf), stdin)){
+        buf[MAX_LINE-1] = '\0'; // add null to end of buf to make c string
+        if (!strncmp(buf, "EXIT", 4)){
+            printf("Peace!\n");
+            break; 
+        }
+        lenSendString = strlen(buf) + 1;
+        if (send(sockFd ,buf, lenSendString, 0) == -1){
+            perror("client send error");
+            close(sockFd);
+            exit(1);
+        }
+    }
+    
+    close(sockFd);
+    printf("END\n");
     return 0;
 } 
--- server/myftpd.c
+++ server/myftpd.c
@@ -11,6 +11,7 @@
 #include<netdb.h>
 #include<time.h>
 #include<sys/time.h>
+#include<unistd.h>
 
 #define BUF_SIZE 4096
 #define BACKLOGN 10      // number of pending connections queue will hold
@@ -27,38 +28,55 @@ int main(int argc, char * argv[]){
     }
     
     // load address structs
-    struct addrinfo hints, *res;
+    struct addrinfo hints, *servInfo, *ptr;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET; // ipv4
     hints.ai_socktype = SOCK_STREAM; // needs to be stream
     hints.ai_flags = AI_PASSIVE; // want to use my local IP, fill it in for me   
 
     // inputs: [IP of named addr, string(PORTNUM),struct addrinfo *,struct addrinfro **] 
-    getaddrinfo(NULL, argv[1], &hints, &res);
+    int returnVal;
+    if ((returnVal = getaddrinfo(NULL, argv[1], &hints, &servInfo)) != 0){
+        fprintf(stderr, "getaddrinfro: %s\n", gai_strerror(returnVal));
+        exit(1);
+    }
 
+    // loop through getaddrinfo results and bind to first
     printf("before bind\n");
-    // make socket
-    if ((sockFd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0){
-        perror("bad socket");
+    for (ptr = servInfo; ptr != NULL; ptr = ptr->ai_next){
+        // make socket
+        if ((sockFd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol)) == -1){
+            perror("bad socket");
+            continue;
+        }
+        // additional socket options
+        if (setsockopt(sockFd, SOL_SOCKET, SO_REUSEADDR, &portReuse, sizeof(portReuse)) == -1){
+            perror("bad setsockopt");
+            exit(1);
+        }  
+        printf("after sock\n");
+        // bind socket 
+        if ((bind(sockFd, ptr->ai_addr, ptr->ai_addrlen) < 0)){
+            close(sockFd);
+            perror("bad bind");
+            continue;
+        }
+        
+        break; // break loop since we found a good bind
+        printf("after bind\n");
+    }
+
+    free(servInfo); // don't need the struct anymore
+    if (ptr == NULL){
+        fprintf(stderr, "failed to bind\n");
         exit(1);
     }
-    // additional socket options
-    if (setsockopt(sockFd, SOL_SOCKET, SO_REUSEADDR, &portReuse, sizeof(portReuse)) == -1){
-        perror("bad setsockopt");
-        exit(1);
-    }
-    
-    printf("after sock\n");
-    // bind socket 
-    if ((bind(sockFd, res->ai_addr, res->ai_addrlen) < 0)){
-        perror("bad bind");
-        exit(1);
-    }
-    printf("after bind\n");
-    
+
     // listen on socket
     if ((listen(sockFd, BACKLOGN)) < 0){
+        close(sockFd);
         perror("bad listen");
+        exit(1);
     }
 
     // accept an incoming connection
@@ -70,6 +88,19 @@ int main(int argc, char * argv[]){
         perror("bad accept");
         exit(1);
     }
+   
+    // after accept, lets start main loop to interact with client
+    int numBytesRec;
+    while(1){
+        if ((numBytesRec=recv(newFd, buf, sizeof(buf),0)) == -1){
+            perror("recieve error");
+            exit(1);
+        }
+        if (numBytesRec == 0) break; // no bytes recieved
+        printf("Server recieverd: %s\n", buf);
+    }
+    
+    close(sockFd);
     printf("END\n");
     return 0;
 }
